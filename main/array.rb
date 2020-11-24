df871bc311275cb929ffd3e3556db1194e098d39
jianghao.zhang <jianghaozhang@qq.com>
2020-11-24 11:49:22 +0800

    refactor

--- main/array.rb
+++ main/array.rb
@@ -1,4 +1,6 @@
-a = [ 2,"dog",3.1415]
+# frozen_string_literal: true
+
+a = [2, 'dog', 3.1415]
 puts a[0]
 puts a[1]
 
@@ -6,11 +8,11 @@ a[2] = nil
 print a
 print "\n"
 
-b = ['first','second','five','infinity']
+b = %w[first second five infinity]
 print b
 print "\n"
 
-# %w 表示数组中的内容均是字符串，后接花括号而不是方括号，而且要注意用空格而不是逗号分隔
-c = %w{first second third}
+# 用空格而不是逗号分隔
+c = %w[first second third]
 print c
 print "\n"
